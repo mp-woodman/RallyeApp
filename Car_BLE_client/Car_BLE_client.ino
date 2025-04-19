a2a17f3c4d8d68c856d9441332cd70dcefb69cf5
mp-woodman <holzi1996@gmx.de>
2025-04-19 22:29:03 +0200

    Darstellungen der Zeiten im Display verbessert, Intervallmessung ergänzt + Build Nodemuc esp32s

--- Car_BLE_client/Car_BLE_client.ino
+++ Car_BLE_client/Car_BLE_client.ino
@@ -8,29 +8,32 @@
 #include <SPI.h>
 
 #include <GxEPD.h>
-#include <GxGDEH029A1/GxGDEH029A1.h>      // 2.9" b/w
+#include <GxGDEH029A1/GxGDEH029A1.h>  // 2.9" b/w
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
-#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeSansBold9pt7b.h>
+#include <Fonts/FreeSans9pt7b.h>
+#include <Fonts/FreeSans12pt7b.h>
 #include <Fonts/FreeMonoBold12pt7b.h>
 
 
 #include "BLEDevice.h"
 
+//compile info Target ESP32S Nodemuc ==> select NodeMUC-32S
 
 //Defines
 #define myLog(content) Serial.print(content)
 #define myLogLn(content) Serial.println(content)
 
 //--------------- pin defines ---------------
-#define PN532_SCK    (14)
-#define PN532_MOSI   (13)
-#define PN532_SS     (15)
-#define PN532_MISO   (12)
+#define PN532_SCK (14)
+#define PN532_MOSI (13)
+#define PN532_SS (15)
+#define PN532_MISO (12)
 
-#define E_PAPER_SS   (5)
-#define E_PAPER_DC   (17)
-#define E_PAPER_RST  (16)
+#define E_PAPER_SS (5)
+#define E_PAPER_DC (17)
+#define E_PAPER_RST (16)
 #define E_PAPER_BUSY (4)
 
 #define BEEP (27)
@@ -38,7 +41,7 @@
 // The remote service we wish to connect to.
 static BLEUUID serviceUUID("723700b4-b295-42b3-9881-b4f0351e3569");
 // The characteristic of the remote service we are interested in.
-static BLEUUID    charUUID("3cc4b861-0818-4dcd-aac8-8c494539bf05");
+static BLEUUID charUUID("3cc4b861-0818-4dcd-aac8-8c494539bf05");
 
 static boolean doConnect = false;
 static boolean connected = false;
@@ -50,55 +53,76 @@ static BLEAdvertisedDevice* myDevice;
 //E-paper
 GxIO_Class io(SPI, E_PAPER_SS, E_PAPER_DC, E_PAPER_RST);
 GxEPD_Class display(io, E_PAPER_RST, E_PAPER_BUSY);
-bool newDataAvailable= false;
-struct timeval lastTime = (struct timeval){ 0 };
-struct timeval duration = (struct timeval){ 0 };
-int currentMeasurementNumber=0;
+bool newDataAvailable = false; 
+struct timeval lastTime {};
+struct timeval duration {};
+struct timeval time_difference_to_even {};
+struct timeval duration_difference_to_even {};
+
+int currentMeasurementNumber = 0;
 int currentDifference = 0;
 long lastEpaperUpdate = 0;
 
-int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y)
+// 'Symbol_Clock', 16x16px
+const unsigned char epd_bitmap_Symbol_Clock [] PROGMEM = {
+	0x07, 0xe0, 0x1e, 0x78, 0x31, 0x8c, 0x61, 0x84, 0x41, 0x82, 0xc1, 0x83, 0xc1, 0x83, 0x81, 0xf1, 
+	0x81, 0xf1, 0xc0, 0x03, 0xc0, 0x03, 0x40, 0x02, 0x20, 0x04, 0x30, 0x0c, 0x1e, 0x78, 0x07, 0xe0
+};
+// 'Symbol_diference', 16x16px
+const unsigned char epd_bitmap_Symbol_diference [] PROGMEM = {
+	0x00, 0x00, 0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0x03, 0xc0, 0x02, 0x40, 0x02, 0x40, 0x06, 0x60, 
+	0x04, 0x20, 0x0c, 0x30, 0x0c, 0x30, 0x08, 0x10, 0x18, 0x18, 0x10, 0x08, 0x1f, 0xf8, 0x3f, 0xfc
+};
+
+
+
+struct timeval calcNextEvenTime(const struct timeval* unevenTime)
 {
-  struct timeval xx = *x;
-  struct timeval yy = *y;
-  x = &xx; y = &yy;
+  struct tm nextEvenTime_tm = *localtime(&unevenTime->tv_sec);
+  const uint32_t roundValue {10};
+  nextEvenTime_tm.tm_sec = (int)round((float)nextEvenTime_tm.tm_sec / roundValue)*roundValue;
 
-  if (x->tv_usec > 999999)
+  if(nextEvenTime_tm.tm_sec >= 60)
   {
-    x->tv_sec += x->tv_usec / 1000000;
-    x->tv_usec %= 1000000;
+      nextEvenTime_tm.tm_sec-= 60;
+      nextEvenTime_tm.tm_min += 1;
   }
 
-  if (y->tv_usec > 999999)
-  {
-    y->tv_sec += y->tv_usec / 1000000;
-    y->tv_usec %= 1000000;
-  }
-
-  result->tv_sec = x->tv_sec - y->tv_sec;
-
-  if ((result->tv_usec = x->tv_usec - y->tv_usec) < 0)
-  {
-    result->tv_usec += 1000000;
-    result->tv_sec--; // borrow
-  }
-
-  return result->tv_sec < 0;
+  struct timeval nextEvenTime {};
+  nextEvenTime.tv_sec = mktime(&nextEvenTime_tm);
+  return nextEvenTime;
 }
 
-void printCurrentTime_us(struct timeval tv_now, struct timeval tv_duration)
+struct timeval differenceToNextEvenTime(const struct timeval& time)
 {
+  //time difference to next even time
+  struct timeval nextEvenTime {calcNextEvenTime(&time)};
+  struct timeval differenceToNextEvenTime{};
+
+  timersub(&time, &nextEvenTime, &differenceToNextEvenTime);
+  if(differenceToNextEvenTime.tv_sec < 0)
+  {
+    differenceToNextEvenTime.tv_usec = 1000000 - differenceToNextEvenTime.tv_usec;
+  }
+  return differenceToNextEvenTime;
+}
+
+void printCurrentTime_us(struct timeval tv_now, struct timeval tv_difference, struct timeval tv_duration) {
   int milli = tv_now.tv_usec / 1000;
 
-  char buffer [80];
-  strftime(buffer, 80, "%d.%m.%Y %H:%M:%S", localtime(&tv_now.tv_sec));
+  char buffer[80];
+  strftime(buffer, 80, "now: %d.%m.%Y %H:%M:%S", localtime(&tv_now.tv_sec));
 
   char currentTime[84] = "";
   sprintf(currentTime, "%s,%03d", buffer, milli);
   myLogLn(currentTime);
 
-  char durationbuffer [80];
-  strftime(durationbuffer, 80, "%d.%m.%Y %H:%M:%S", localtime(&tv_now.tv_sec));
+  char differencebuffer[80];
+  sprintf(differencebuffer, "dif: %ld,%ld", (long)tv_difference.tv_sec, (long)tv_difference.tv_usec);
+  myLogLn(differencebuffer);
+
+  char durationbuffer[80];
+  strftime(durationbuffer, 80, "dur: %d.%m.%Y %H:%M:%S", localtime(&tv_duration.tv_sec));
   char duration[84] = "";
   int durationmilli = tv_duration.tv_usec / 1000;
   sprintf(duration, "%s,%03d", durationbuffer, durationmilli);
@@ -110,19 +134,24 @@ static void notifyCallback(
   uint8_t* pData,
   size_t length,
   bool isNotify) {
-    digitalWrite(BEEP, true);
-    delay(200);
-    digitalWrite(BEEP, false);
-    
-    struct timeval newTime = *(struct timeval*)pData;
-    if (lastTime.tv_usec != 0 && lastTime.tv_sec != 0)
-    {
-      timeval_subtract(&duration, &newTime, &lastTime);
-    }
-    lastTime = newTime;
-    currentMeasurementNumber++;
-    printCurrentTime_us(lastTime, duration);
-    newDataAvailable = true;
+  digitalWrite(BEEP, true);
+  delay(200);
+  digitalWrite(BEEP, false);
+
+  //time measurement
+  struct timeval newTime = *(struct timeval*)pData;
+  //time duration sinc last measurement
+  if (lastTime.tv_usec != 0 && lastTime.tv_sec != 0) {
+    timersub(&newTime, &lastTime, &duration);
+  }
+  time_difference_to_even = differenceToNextEvenTime(newTime);
+  duration_difference_to_even = differenceToNextEvenTime(duration);
+
+  lastTime = newTime;
+  currentMeasurementNumber++;
+  printCurrentTime_us(lastTime, time_difference_to_even, duration);
+
+  newDataAvailable = true;
 }
 
 class MyClientCallback : public BLEClientCallbacks {
@@ -136,58 +165,58 @@ class MyClientCallback : public BLEClientCallbacks {
 };
 
 bool connectToServer() {
-    Serial.print("Forming a connection to ");
-    Serial.println(myDevice->getAddress().toString().c_str());
-    
-    BLEClient*  pClient  = BLEDevice::createClient();
-    Serial.println(" - Created client");
+  Serial.print("Forming a connection to ");
+  Serial.println(myDevice->getAddress().toString().c_str());
 
-    pClient->setClientCallbacks(new MyClientCallback());
+  BLEClient* pClient = BLEDevice::createClient();
+  Serial.println(" - Created client");
 
-    // Connect to the remove BLE Server.
-    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
-    Serial.println(" - Connected to server");
-    pClient->setMTU(517); //set client to request maximum MTU from server (default is 23 otherwise)
-  
-    // Obtain a reference to the service we are after in the remote BLE server.
-    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-    if (pRemoteService == nullptr) {
-      Serial.print("Failed to find our service UUID: ");
-      Serial.println(serviceUUID.toString().c_str());
-      pClient->disconnect();
-      return false;
-    }
-    Serial.println(" - Found our service");
+  pClient->setClientCallbacks(new MyClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  Serial.println(" - Connected to server");
+  pClient->setMTU(517);  //set client to request maximum MTU from server (default is 23 otherwise)
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our service");
 
 
-    // Obtain a reference to the characteristic in the service of the remote BLE server.
-    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-    if (pRemoteCharacteristic == nullptr) {
-      Serial.print("Failed to find our characteristic UUID: ");
-      Serial.println(charUUID.toString().c_str());
-      pClient->disconnect();
-      return false;
-    }
-    Serial.println(" - Found our characteristic");
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(charUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our characteristic");
 
-    // Read the value of the characteristic.
-    if(pRemoteCharacteristic->canRead()) {
-      String value = pRemoteCharacteristic->readValue();
-      Serial.print("The characteristic value was: ");
-      //Serial.println(value);
-    }
-    Serial.println(" - characteristic check finished");
-    if(pRemoteCharacteristic->canNotify())
-      pRemoteCharacteristic->registerForNotify(notifyCallback);
+  // Read the value of the characteristic.
+  if (pRemoteCharacteristic->canRead()) {
+    String value = pRemoteCharacteristic->readValue();
+    Serial.print("The characteristic value was: ");
+    //Serial.println(value);
+  }
+  Serial.println(" - characteristic check finished");
+  if (pRemoteCharacteristic->canNotify())
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
 
-    connected = true;
-    return true;
+  connected = true;
+  return true;
 }
 /**
  * Scan for BLE servers and find the first one that advertises the service we are looking for.
  */
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
- /**
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  /**
    * Called for each advertising BLE server.
    */
   void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -202,9 +231,9 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       doConnect = true;
       doScan = true;
 
-    } // Found our server
-  } // onResult
-}; // MyAdvertisedDeviceCallbacks
+    }  // Found our server
+  }    // onResult
+};     // MyAdvertisedDeviceCallbacks
 
 
 void setup() {
@@ -216,18 +245,18 @@ void setup() {
 
   display.init();
   display.eraseDisplay();
-  showSetupScreen(&FreeMonoBold9pt7b);
+  showSetupScreen();
   delay(1000);
 
-  
+
   //BLE init & Powersettings
   BLEDevice::init("");
-  esp_err_t errRc=esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT,ESP_PWR_LVL_P9);
+  esp_err_t errRc = esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9);
   esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
-  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN ,ESP_PWR_LVL_P9); 
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN, ESP_PWR_LVL_P9);
 
 
-  showBLEDisconected(&FreeMonoBold9pt7b);
+  showBLEDisconected();
 
   // Retrieve a Scanner and set the callback we want to use to be informed when we
   // have detected a new device.  Specify that we want active scanning and start the
@@ -238,22 +267,22 @@ void setup() {
   pBLEScan->setWindow(449);
   pBLEScan->setActiveScan(true);
   pBLEScan->start(5, false);
-} // End of setup.
+}  // End of setup.
 
 
 // This is the Arduino main loop function.
 void loop() {
 
   // If the flag "doConnect" is true then we have scanned for and found the desired
-  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
   // connected we set the connected flag to be true.
   if (doConnect == true) {
     if (connectToServer()) {
       Serial.println("We are now connected to the BLE Server.");
-      showBLEConected(&FreeMonoBold9pt7b);
+      showBLEConected();
     } else {
       Serial.println("We have failed to connect to the server; there is nothin more we will do.");
-      showBLEFailed(&FreeMonoBold9pt7b);
+      showBLEFailed();
     }
     doConnect = false;
   }
@@ -261,108 +290,127 @@ void loop() {
   // If we are connected to a peer BLE Server, update the characteristic each time we are reached
   // with the current time since boot.
   if (connected) {
-    if (newDataAvailable)
-    {
-      showMeasurementScreen(&FreeMonoBold12pt7b, currentMeasurementNumber, lastTime, duration);
+    if (newDataAvailable) {
+      showMeasurementScreen(currentMeasurementNumber, lastTime, time_difference_to_even, duration, duration_difference_to_even);
       newDataAvailable = false;
     }
-  }else if(doScan){
-    showBLEDisconected(&FreeMonoBold9pt7b);
+  } else if (doScan) {
+    showBLEDisconected();
     BLEDevice::getScan()->start(0);  // this is just example to start scan after disconnect, most likely there is better way to do it in arduino
   }
-  
-  delay(1000); // Delay a second between loops.
-} // End of loop
+
+  delay(1000);  // Delay a second between loops.
+}  // End of loop
 
 
-void showSetupScreen(const GFXfont* f)
-{
+void showSetupScreen() {
   display.fillScreen(GxEPD_WHITE);
   display.setTextColor(GxEPD_BLACK);
-  display.setFont(f);
   display.setCursor(0, 0);
+  display.setFont(&FreeSans12pt7b);
   display.println();
-  display.print("Rally ESP");
+  display.println("Rallye App");
+  display.setFont(&FreeSans9pt7b);
   display.println();
   display.println("startet...");
   display.update();
 }
 
-void showBLEDisconected(const GFXfont* f)
-{
+void showBLEDisconected() {
   display.fillScreen(GxEPD_WHITE);
   display.setTextColor(GxEPD_BLACK);
-  display.setFont(f);
   display.setCursor(0, 0);
+  display.setFont(&FreeSans12pt7b);
   display.println();
-  display.print("Rally ESP");
+  display.println("Rallye App");
+  display.setFont(&FreeSans9pt7b);
   display.println();
-  display.println("no BLE");
-  display.println("try to connect");
+  display.println("Verbinde mit");
+  display.println("Lichtschranke");
   display.println("...");
   display.update();
 }
 
-void showBLEConected(const GFXfont* f)
-{
+void showBLEConected() {
   display.fillScreen(GxEPD_WHITE);
   display.setTextColor(GxEPD_BLACK);
-  display.setFont(f);
   display.setCursor(0, 0);
+  display.setFont(&FreeSans12pt7b);
   display.println();
-  display.print("Rally ESP");
+  display.println("Rallye App");
+  display.setFont(&FreeSans9pt7b);
   display.println();
-  display.println("BLE");
-  display.println("connected");
+  display.println("Lichtschranke");
+  display.println("verbunden");
   display.update();
 }
 
-void showBLEFailed(const GFXfont* f)
-{
+void showBLEFailed() {
   display.fillScreen(GxEPD_WHITE);
   display.setTextColor(GxEPD_BLACK);
-  display.setFont(f);
   display.setCursor(0, 0);
+  display.setFont(&FreeSans12pt7b);
   display.println();
-  display.print("Rally ESP");
+  display.println("Rallye App");
+  display.setFont(&FreeSans9pt7b);
   display.println();
-  display.println("BLE");
-  display.println("failed");
+  display.println("Fehlerhafte");
+  display.println("Verbindung");
+  display.println("mit Lichtschranke");
   display.update();
 }
-
-void showMeasurementScreen(const GFXfont* f, int measurementNbr, struct timeval drivethroughTime, struct timeval durationTime)
+void displayTimeAndDiference(struct timeval& time,struct timeval& diference)
 {
+  display.setFont(&FreeSans9pt7b);
+
+  display.drawBitmap(display.getCursorX(), display.getCursorY() - 14, epd_bitmap_Symbol_Clock, 16, 16, GxEPD_BLACK);
+  display.setCursor(display.getCursorX()+20, display.getCursorY());
+
+  char time_h_m_s_char[20];
+  strftime(time_h_m_s_char, sizeof(time_h_m_s_char), "%H:%M:%S", localtime(&time.tv_sec));
+  int milli = time.tv_usec / 1000;
+  display.printf("%s,%03d\n", time_h_m_s_char, milli);
+
+  display.setFont(&FreeMonoBold12pt7b);
+  display.drawBitmap(display.getCursorX(), display.getCursorY() - 16, epd_bitmap_Symbol_diference, 16, 16, GxEPD_BLACK);
+  display.setCursor(display.getCursorX()+20, display.getCursorY());
+  if(diference.tv_sec >= 0)
+  {
+    display.print("+");
+    display.printf("%ld,%03ld\n",(int32_t)diference.tv_sec, (int32_t)(diference.tv_usec/1000));
+  }
+  else if(diference.tv_sec < 0)
+  {
+     //bei negativen zahlen muss sekundenwert +1 gerechnet werden
+    if(diference.tv_sec == -1)
+    {
+      //spezialfall da sonst zwischen -0,001 und -0,999s kein - angezeigt wird
+      display.print("-");
+    }
+    display.printf("%ld,%03ld\n",(int32_t)diference.tv_sec + 1, (int32_t)(diference.tv_usec/1000));
+  }
+}
+
+void showMeasurementScreen(int measurementNbr, struct timeval drivethroughTime, struct timeval drivethroughDifference, struct timeval durationTime, struct timeval durationDifference) {
   display.eraseDisplay();
   display.fillScreen(GxEPD_WHITE);
   display.setTextColor(GxEPD_BLACK);
-  display.setFont(f);
   display.setCursor(0, 0);
+  display.setFont(&FreeSans12pt7b);
   display.println();
-  display.println("Messung:");
-  display.println(measurementNbr);
-  display.println();
+  display.println("Rallye App");
+  display.setFont(&FreeSans9pt7b);
+  display.printf("Nummer: %d\n", measurementNbr);
   display.println();
   //time
-  display.println("um:");
-  char time_char[20];
-  strftime(time_char, sizeof(time_char), "%H:%M:%S", localtime(&drivethroughTime.tv_sec));
-  display.print(time_char);
-  display.print(",");
-  int milli = drivethroughTime.tv_usec / 1000;
-  char currentTime[20] = "";
-  sprintf(currentTime, "%03d", milli);
-  display.println(currentTime);
+  display.setFont(&FreeSans9pt7b);
+  display.println("Durchfahrt:");
+  displayTimeAndDiference(drivethroughTime, drivethroughDifference);
+
   //duration
-  display.println("delta:");
-  char duration_char[20];
-  strftime(duration_char, sizeof(duration_char), "%H:%M:%S", localtime(&durationTime.tv_sec));
-  display.print(duration_char);
-  display.print(",");
-  int durationmilli = durationTime.tv_usec / 1000;
-  char durationTime_char[20] = "";
-  sprintf(durationTime_char, "%03d", durationmilli);
-  display.println(durationTime_char);
+  display.setFont(&FreeSans9pt7b);
+  display.println();
+  display.println("Intervall:");
+  displayTimeAndDiference(durationTime, durationDifference);
   display.update();
 }
-
